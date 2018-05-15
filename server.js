6c56d481b79653d4e34c42016d940633336c19dd
Arnab Datta <arnabkum@gmail.com>
2018-05-16 00:28:35 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
 import SuitsAppSchema from './src/schema'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
+
 app.use('/graphql', graphqlHTTP({
     schema: SuitsAppSchema,
     graphiql: true
